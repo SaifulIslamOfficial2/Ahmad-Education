c9b403eafea0f987feb09f0c156377b07e121bf4
saifulislam <mdsaifulofficial2@gmail.com>
2024-10-07 23:36:37 +0600

    bangladesi project

---
+++ Asset/js/custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.slider-container').slick({
+        autoplay: true,
+        autoplaySpeed: 1000,
+        arrows: true,
+        slidestoShow: 2,
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fa-solid fa-angle-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fa-solid fa-chevron-right"></i></button>'
+
+        
+    });
+  });
