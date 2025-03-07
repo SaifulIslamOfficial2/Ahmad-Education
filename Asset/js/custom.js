87c109a7995a1a802effd29c34e4ed8a1852b39d
saifulislam <mdsaifulofficial2@gmail.com>
2025-03-07 17:46:07 +0600

    update

--- Asset/js/custom.js
+++ Asset/js/custom.js
@@ -11,3 +11,72 @@ $(document).ready(function(){
         
     });
   });
+
+  
+        // Toggle Mobile Menu
+        document.getElementById('nav-toggle').addEventListener('click', function() {
+            var mobileNav = document.getElementById('mobile-nav');
+            mobileNav.classList.toggle('hidden');
+        });
+    
+
+
+
+        //  new 
+
+       
+
+    let currentSlide = 0;
+
+    function moveToSlide(slideIndex) {
+        const carousel = document.getElementById('carousel');
+        const slideWidth = document.querySelector('.testimonial').offsetWidth;
+        carousel.style.transform = `translateX(-${slideWidth * slideIndex}px)`;
+        currentSlide = slideIndex;
+    }
+
+    // Auto-slide functionality (optional)
+    // setInterval(() => {
+    //     currentSlide = (currentSlide + 1) % 3;
+    //     moveToSlide(currentSlide);
+    // }, 2000); // Change slide every 5 seconds
+
+
+
+      
+      var swiper = new Swiper(".progress-slide-carousel", {
+      loop: true,
+      fraction: true,
+      autoplay: {
+        delay: 1200,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".progress-slide-carousel .swiper-pagination",
+        type: "progressbar",
+      },
+      });
+// ===================================
+
+// <!--JAVASCRIPT CODE-->
+    
+      var swiper = new Swiper(".fraction-slide-carousel", {
+      loop: true,
+      fraction: true,
+      navigation: {
+        nextEl: ".fraction-slide-carousel .swiper-button-next",
+        prevEl: ".fraction-slide-carousel .swiper-button-prev",
+      },
+      pagination: {
+        el: '.fraction-slide-carousel .swiper-pagination',
+        type: 'fraction',
+        formatFractionCurrent: function(number) {
+          return number;
+        }
+      },
+      });
+      
+
+      //  swiper
+
+  
